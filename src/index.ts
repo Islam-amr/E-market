9cc56581972c2f19f0132901ef0631cfcee7f538
Islam-amr <islam.amr1997@gmail.com>
2023-08-24 16:58:17 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.use('/', (req, res, next) => {
-    res.send('<h1>Som3aaa</h1>')
+    res.send('<h1>Hello node js</h1>')
 })
 
 app.listen(3000)
